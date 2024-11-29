5d6d0f4ba05f3beee1c9106194b70ffed0e697af
jefferson.leal <jefferson.leal@7comm.com.br>
2024-11-29 15:53:12 -0300

    Setando valores para retornar pelo mock

--- automocao-teste-mockito/src/test/java/br/com/jeffersondevjava/negocio/CursoNegocioMockTest.java
+++ automocao-teste-mockito/src/test/java/br/com/jeffersondevjava/negocio/CursoNegocioMockTest.java
@@ -1,7 +1,10 @@
 package br.com.jeffersondevjava.negocio;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,19 +17,33 @@ class CursoNegocioMockTest {
 
 	CursoServico mockServico;
 	CursoNegocio negocio;
+	List<String> cursos;
 	
 	@BeforeEach
-	void stup() {
+	void setup() {
 		// Given
-		mockServico = new CursoServiceStubsTest();
+		mockServico = mock(CursoServico.class);
 		negocio = new CursoNegocio(mockServico);
+		
+		cursos = Arrays.asList(
+                "REST API's RESTFul do 0 à Azure com ASP.NET Core 5 e Docker",
+                "Agile Desmistificado com Scrum, XP, Kanban e Trello",
+                "Spotify Engineering Culture Desmistificado",
+                "REST API's RESTFul do 0 à AWS com Spring Boot 3 Java e Docker",
+                "Docker do Zero à Maestria - Contêinerização Desmistificada",
+                "Docker para Amazon AWS Implante Apps Java e .NET com Travis CI",
+                "Microsserviços do 0 com Spring Cloud, Spring Boot e Docker",
+                "Arquitetura de Microsserviços do 0 com ASP.NET, .NET 6 e C#",
+                "REST API's RESTFul do 0 à AWS com Spring Boot 3 Kotlin e Docker",
+                "Kotlin para DEV's Java: Aprenda a Linguagem Padrão do Android",
+                "Microsserviços do 0 com Spring Cloud, Kotlin e Docker"
+            );
 	}
 	
 	@Test
 	void testCursoRelacionadoSpring_Quando_UsamosMock() {
-
-		CursoServico mockServico = mock(CursoServico.class);
-		CursoNegocio negocio = new CursoNegocio(mockServico);
+		
+		when(mockServico.recuperarCursos("Jefferson")).thenReturn(cursos);
 
 		// when
 		var filtroCursos = negocio.recuperarCursosSobreSpring("Jefferson");
